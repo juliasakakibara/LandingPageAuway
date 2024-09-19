ebe8b359e47128e00becfb704b6ad5880831bae1
juliasakakibara <juliasakakibara.o@gmail.com>
2024-09-18 21:50:38 -0300

    added anchor

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Seletores de Botões
-    const btnEN = document.getElementById('btn-en');
-    const btnPT = document.getElementById('btn-pt');
-
-    // Função para mudar o idioma
-    function switchLanguage(lang) {
-        if (lang === 'en') {
-            document.documentElement.lang = 'en';
-            document.getElementById('sobre').innerHTML = "<h1>About</h1><p>Auwau group</p>";
-            document.getElementById('contato').innerHTML = "<h2>Contact</h2><p>Email: <a href='mailto:email@exemplo.com'>email@exemplo.com</a></p>";
-        } else {
-            document.documentElement.lang = 'pt-br';
-            document.getElementById('sobre-mim').innerHTML = "<h1>Sobre Mim</h1><p>Sou Julia Sakakibara, uma designer de produto digital...</p>";
-            document.getElementById('contato').innerHTML = "<h2>Contato</h2><p>Email: <a href='mailto:email@exemplo.com'>email@exemplo.com</a></p>";
-        }
-    }
-
-    // Event Listeners para Mudança de Idioma
-    btnEN.addEventListener('click', function () {
-        switchLanguage('en'); 
-    }); 
-
-    btnPT.addEventListener('click', function () {
-        switchLanguage('pt');
+// Smooth Scroll for Internal Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
     });
-});
+  });
+  
